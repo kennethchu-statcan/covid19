68db2b05ae2703e653fecfebfa3dbfe24783f600
paradisepilot <paradisepilot@gmail.com>
2020-04-11 09:31:16 -0400

    added: 004-diagnostics/

---
+++ 004-diagnostics/code/main.R
@@ -0,0 +1,82 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+
+# add custom library using .libPaths()
+print( data.directory   );
+print( code.directory   );
+print( output.directory );
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+# source supporting R code
+code.files <- c(
+    "cross-check.R",
+    "getData-raw.R"
+    );
+
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+require(rstan);
+require(data.table);
+require(lubridate);
+require(gdata);
+require(EnvStats);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+#data.snapshot <- "imperial-data-1.0";
+#data.snapshot  <- "2020-04-05.01";
+#data.snapshot  <- "2020-04-07.01";
+#data.snapshot  <- "2020-04-11.01";
+data.snapshot  <- "2020-04-11.02";
+data.directory <- file.path(data.directory,data.snapshot);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+set.seed(7654321);
+
+list.raw.data <- getData.raw(
+    csv.ECDC        = file.path(data.directory,'raw-covid19-ECDC.csv'),
+    csv.JHU.cases   = file.path(data.directory,'raw-covid19-JHU-cases.csv'),
+    csv.JHU.deaths  = file.path(data.directory,'raw-covid19-JHU-deaths.csv'),
+    csv.GoCInfobase = file.path(data.directory,'raw-covid19-GoCInfobase.csv')
+    );
+
+print( names(list.raw.data) );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+DF.cross.check.JHU.GoCInfobase <- cross.check.JHU.GoCInfobase(
+    list.raw.data = list.raw.data
+    );
+print(str(DF.cross.check.JHU.GoCInfobase));
+
+DF.cross.check.JHU.ECDC <- cross.check.JHU.ECDC(
+    list.raw.data = list.raw.data
+    );
+print(str(DF.cross.check.JHU.ECDC));
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
+
