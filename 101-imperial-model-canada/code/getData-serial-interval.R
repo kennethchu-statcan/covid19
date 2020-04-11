1f14b14c369753cf095e9caf0772381f9baf243b
paradisepilot <paradisepilot@gmail.com>
2020-04-11 17:01:01 -0400

    updated: getData-serial-interval.R

--- 101-imperial-model-canada/code/getData-serial-interval.R
+++ 101-imperial-model-canada/code/getData-serial-interval.R
@@ -1,7 +1,7 @@
 
 getData.serial.interval <- function(
-    CSV.serial.interval = NULL,
-    RData.output        = "input-serial-interval.RData"
+    csv.serial.interval = NULL,
+    csv.output          = "input-serial-interval.csv"
     ) {
 
     thisFunctionName <- "getData.serial.interval";
@@ -13,21 +13,20 @@ getData.serial.interval <- function(
     require(readr);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    if ( file.exists(RData.output) ) {
+    if ( file.exists(csv.output) ) {
 
-        cat(paste0("\n### ",RData.output," already exists; loading this file ...\n"));
-
-        DF.output <- readRDS(file = RData.output);
-
-        cat(paste0("\n### Finished loading raw data.\n"));
+        cat(paste0("\n# The data file ",csv.output," already exists; loading this file ...\n"));
+        DF.output <- read.csv(file = csv.output, stringsAsFactors = FALSE, na.strings = c("NA","N/A"));
+        cat(paste0("\n# Loading complete: ",csv.output,"\n"));
 
     } else {
 
-        DF.output <- read.csv( CSV.serial.interval );
-
-        if (!is.null(RData.output)) {
-            saveRDS(object = DF.output, file = RData.output);
-            }
+        DF.output <- read.csv( file = csv.serial.interval, stringsAsFactors = FALSE );
+        write.csv(
+            x         = DF.output,
+            file      = csv.output,
+            row.names = FALSE
+            );
 
         }
 
