cca0f784865c60568e8a013ccbff24719a9e1e7b
paradisepilot <paradisepilot@gmail.com>
2020-04-10 21:29:31 -0400

    updated: cross-check.R

--- 004-imperial-model-canada/code/cross-check.R
+++ 004-imperial-model-canada/code/cross-check.R
@@ -1,12 +1,12 @@
 
-cross.check.JHU.GoCInfobase <- function(
+cross.check.JHU.ECDC <- function(
     retained.jurisdictions = NULL,
     JHU.url.cases          = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
     JHU.url.deaths         = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
     GoCInfobase.url        = "https://health-infobase.canada.ca/src/data/covidLive/covid19.csv"
     ) {
 
-    thisFunctionName <- "cross.check.JHU.GoCInfobase";
+    thisFunctionName <- "cross.check.JHU.ECDC";
     cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
     cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
 
@@ -89,8 +89,98 @@ cross.check.JHU.GoCInfobase <- function(
 
     }
 
+cross.check.JHU.GoCInfobase <- function(
+    retained.jurisdictions = NULL,
+    JHU.url.cases          = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+    JHU.url.deaths         = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
+    GoCInfobase.url        = "https://health-infobase.canada.ca/src/data/covidLive/covid19.csv"
+    ) {
+
+    thisFunctionName <- "cross.check.JHU.GoCInfobase";
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    require(lubridate);
+    require(readr);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.JHU.cases <- cross.check_download(
+        target.url = JHU.url.cases
+        );
+
+    DF.JHU.deaths <- cross.check_download(
+        target.url = JHU.url.deaths
+        );
+
+    DF.JHU.cases  <- DF.JHU.cases[ DF.JHU.cases[, "Country.Region"] == "Canada",];
+    DF.JHU.deaths <- DF.JHU.deaths[DF.JHU.deaths[,"Country.Region"] == "Canada",];
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.GoCInfobase <- cross.check_download(
+        target.url = GoCInfobase.url
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.JHU.cases <- cross.check_reformat.JHU(
+        DF.input      = DF.JHU.cases,
+        colname.value = "cases.JHU"
+        );
+
+    DF.JHU.deaths <- cross.check_reformat.JHU(
+        DF.input      = DF.JHU.deaths,
+        colname.value = "deaths.JHU"
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.GoCInfobase <- cross.check_reformat.GoCInfobase(
+        DF.input = DF.GoCInfobase
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.JHU <- dplyr::full_join(
+        x  = DF.JHU.cases,
+        y  = DF.JHU.deaths,
+        by = c("jurisdiction","date")
+        );
+
+    DF.JHU <- DF.JHU %>% dplyr::arrange(jurisdiction,date);
+    DF.JHU <- as.data.frame(DF.JHU);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.cross.check <- dplyr::full_join(
+        x  = DF.JHU,
+        y  = DF.GoCInfobase,
+        by = c("jurisdiction","date")
+        );
+
+    DF.cross.check <- DF.cross.check %>% dplyr::arrange(jurisdiction,date);
+    DF.cross.check <- as.data.frame(DF.cross.check);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    colnames.integer <- setdiff(colnames(DF.cross.check),c("jurisdiction","date")); 
+    for ( temp.colname in colnames.integer) {
+        temp.vector <- DF.cross.check[,temp.colname];
+        temp.vector[is.na(temp.vector)] <- 0;
+        DF.cross.check[,temp.colname] <- temp.vector;
+        }
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    write.csv(
+        x         = DF.cross.check,
+        file      = "diagnostics-compare-JHU-GoCInfobase.csv",
+        row.names = FALSE
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n",thisFunctionName,"() quits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( DF.cross.check );
+
+    }
+
 ##################################################
-cross.check.JHU.GoCInfobase_reformat.GoCInfobase <- function(
+cross.check_reformat.GoCInfobase <- function(
     DF.input = NULL
     ) {
     require(dplyr);
@@ -111,7 +201,7 @@ cross.check.JHU.GoCInfobase_reformat.GoCInfobase <- function(
     return( DF.output );
     }
 
-cross.check.JHU.GoCInfobase_reformat.JHU <- function(
+cross.check_reformat.JHU <- function(
     DF.input      = NULL,
     colname.value = NULL
     ) {
@@ -203,7 +293,7 @@ cross.check.JHU.GoCInfobase_reformat.JHU <- function(
 
     }
 
-cross.check.JHU.GoCInfobase_download <- function(
+cross.check_download <- function(
     target.url = NULL
     ) {
     temp.file <- "temp.csv";
