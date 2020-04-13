1f168d0c261482a970b3fa7042c17ae7e8239450
paradisepilot <paradisepilot@gmail.com>
2020-04-13 03:21:14 -0400

    updated: wrapper-stan.R

--- 101-imperial-model-canada/code/wrapper-stan.R
+++ 101-imperial-model-canada/code/wrapper-stan.R
@@ -348,6 +348,7 @@ wrapper.stan_inner <- function(
     if(DEBUG) {
         fit <- rstan::sampling(object = m, data = stan_data, iter = 40, warmup = 20, chains = 2);
     } else {
+
         # fit = rstan::sampling(
         #     object  = m,
         #     data    = stan_data,
@@ -357,15 +358,27 @@ wrapper.stan_inner <- function(
         #     thin    = 4,
         #     control = list(adapt_delta = 0.90, max_treedepth = 10)
         #     );
-        fit <- rstan::sampling(
+
+        #fit <- rstan::sampling(
+        #    object  = m,
+        #    data    = stan_data,
+        #    iter    = 200,
+        #    warmup  = 100,
+        #    chains  = 4,
+        #    thin    = 4,
+        #    control = list(adapt_delta = 0.90, max_treedepth = 10)
+        #    );
+
+        fit = rstan::sampling(
             object  = m,
             data    = stan_data,
-            iter    = 200,
-            warmup  = 100,
+            iter    = 4000,
+            warmup  = 2000,
             chains  = 4,
             thin    = 4,
             control = list(adapt_delta = 0.90, max_treedepth = 10)
             );
+
         }
 
     out                 <- rstan::extract(fit);
