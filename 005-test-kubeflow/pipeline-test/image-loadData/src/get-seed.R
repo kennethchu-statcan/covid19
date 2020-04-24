f6f69dc79e33c5d56d509a9f77d2b25e8f798899
paradisepilot <paradisepilot@gmail.com>
2020-04-24 13:50:54 -0400

    updated: get-seed.R

--- 005-test-kubeflow/pipeline-test/image-loadData/src/get-seed.R
+++ 005-test-kubeflow/pipeline-test/image-loadData/src/get-seed.R
@@ -12,7 +12,7 @@ get.seed <- function(
     # output.seed <- jsonlite::read_json(input.json)[["seed"]];
 
     my.system.command <- paste0('egrep "seed" ',input.json);
-    temp              <- system(command = my.system.command);
+    temp              <- system(command = my.system.command, intern = TRUE);
     temp              <- gsub(x = temp, pattern = '(\"|A-Za-z|:)', replacement = '');
     output.seed       <- as.integer(temp);
 
