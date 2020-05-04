6c8d071e6dcee5e43b1b9a3d71166718df022c7c
paradisepilot <paradisepilot@gmail.com>
2020-05-04 02:06:23 -0400

    added: 104-imperial-model-canada/

---
+++ 104-imperial-model-canada/run-main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+currentDIR=`pwd`
+   codeDIR=${currentDIR}/code
+ outputDIR=${currentDIR}/output
+
+parentDIR=`dirname ${currentDIR}`
+  dataDIR=${parentDIR}/000-data
+
+if [ ! -d ${outputDIR} ]; then
+	mkdir -p ${outputDIR}
+fi
+
+cp -r ${codeDIR} ${outputDIR}
+cp    $0         ${outputDIR}/code
+
+##################################################
+myRscript=${codeDIR}/main.R
+stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
+stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
+R --no-save --args ${dataDIR} ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+
+##################################################
+exit
+
