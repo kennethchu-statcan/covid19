04b247de9971a1ec0949cdb995027da7dcd9bc2a
paradisepilot <paradisepilot@gmail.com>
2020-05-06 17:14:22 -0400

    updated: visualize-estimates.R

--- 008-ifr-sensitivity/code/visualize-estimates.R
+++ 008-ifr-sensitivity/code/visualize-estimates.R
@@ -24,11 +24,40 @@ visualize.estimates_alpha <- function(
     DF.input = NULL
     ) {
 
+    require(ggplot2);
+
     temp.covariates <- unique(DF.input[,"covariate"]);
     for ( temp.covariate in temp.covariates ) {
+
         DF.temp <- DF.input[DF.input[,"covariate"] == temp.covariate,];
         cat("\nDF.temp\n");
         print( DF.temp   );
+
+        my.ggplot <- initializePlot(
+            title    = NULL,
+            subtitle = gsub(x=temp.covariate,pattern="\\.",replacement=" ")
+            );
+
+        my.ggplot <- my.ggplot + scale_x_continuous(limits=c(-0.05,1.05),breaks=seq(0,1,0.1));
+
+        my.ggplot <- my.ggplot + geom_histogram(
+            data    = DF.temp,
+            mapping = aes(x = posterior.mean),
+            #size    = 0.2,
+            alpha   = 0.3
+            );
+
+        temp.string <- gsub(x=temp.covariate,pattern="\\.",replacement="-");
+        PNG.output  <- paste0("plot-alpha-",temp.string,".png");
+        ggsave(
+            file   = PNG.output,
+            plot   = my.ggplot,
+            dpi    = 300,
+            height =   8,
+            width  =  10,
+            units  = 'in'
+            );
+
         }
 
     return( NULL );
