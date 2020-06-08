628d4c3e5e5c0664c2fac18b74e798cbbc550af3
paradisepilot <paradisepilot@gmail.com>
2020-06-08 03:08:48 -0400

    updated: code/visualizeData-Ottawa.R supplementary/plot-ottawa-hospitalization.png; added: supplementary/plot-ottawa-hospitalization-with-cumsum-admission.png

--- 009-ottawa/code/visualizeData-Ottawa.R
+++ 009-ottawa/code/visualizeData-Ottawa.R
@@ -27,13 +27,16 @@ visualizeData.Ottawa_by.date <- function(
     DF.input = NULL
     ) {
 
+    DF.plot <- DF.input;
+    DF.plot[,"cumsum_admission"] <- base::cumsum(DF.plot[,"admission"]);
+
     my.ggplot <- initializePlot(
         title    = NULL,
         subtitle = 'Ottawa COVID-19 hospitalization data',
         );
 
     my.ggplot <- my.ggplot + geom_line(
-        data    = DF.input,
+        data    = DF.plot,
         mapping = aes(x = date, y = hospitalized),
         alpha   = 0.80,
         size    = 0.75,
@@ -41,7 +44,7 @@ visualizeData.Ottawa_by.date <- function(
         );
 
     my.ggplot <- my.ggplot + geom_col(
-        data    = DF.input,
+        data    = DF.plot,
         mapping = aes(x = date, y = admission),
         alpha   = 0.50,
         size    = 0.75,
@@ -50,15 +53,33 @@ visualizeData.Ottawa_by.date <- function(
         );
 
     my.ggplot <- my.ggplot + geom_col(
-        data    = DF.input,
+        data    = DF.plot,
         mapping = aes(x = date, y = -1 * discharge),
         alpha   = 0.50,
         size    = 0.75,
         width   = 0.50,
-        colour  = "black"
+        colour  = "blue"
         );
 
     PNG.output  <- paste0("plot-ottawa-hospitalization.png");
+    ggsave(
+        file   = PNG.output,
+        plot   = my.ggplot,
+        dpi    = 300,
+        height =   8,
+        width  =  16,
+        units  = 'in'
+        );
+
+    my.ggplot <- my.ggplot + geom_line(
+        data    = DF.plot,
+        mapping = aes(x = date, y = cumsum_admission),
+        alpha   = 0.80,
+        size    = 0.75,
+        colour  = "red"
+        );
+
+    PNG.output  <- paste0("plot-ottawa-hospitalization-with-cumsum-admission.png");
     ggsave(
         file   = PNG.output,
         plot   = my.ggplot,
