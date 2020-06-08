9d3831ee163c5637b88550c65c4bc18a01cb1815
paradisepilot <paradisepilot@gmail.com>
2020-06-07 23:55:47 -0400

    added: 2020-06-07.01/

---
+++ 000-data/2020-06-07.01/run-wget.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://www.arcgis.com/sharing/rest/content/items/df277546e2904acfaf1a61cacb5b5d3c/data
+
+mv    data  Covid19_CODOttawaResidentHospitalAdmissionsByDay_EN.xlsx
+chmod ugo-w Covid19_CODOttawaResidentHospitalAdmissionsByDay_EN.xlsx
+
