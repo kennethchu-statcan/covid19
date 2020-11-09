4bb1cd50c44b74aa86edb15ab20f95a810b93244
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-11-09 17:19:49 -0500

    updated: dashboard-dummy-03.Rmd main.R

--- 301-admissions-ottawa/code/main.R
+++ 301-admissions-ottawa/code/main.R
@@ -79,50 +79,9 @@ file.copy(
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 set.seed(7654321);
 
-data.snapshot <- "2020-11-04.01";
-DF.ottawa <- getData.Ottawa(
-    csv.input = file.path(data.directory,data.snapshot,"raw-covid19-Ottawa.csv")
-    );
-print( str(DF.ottawa) );
-print( summary(DF.ottawa) );
-
-DF.ottawa.plot <- DF.ottawa;
-
-# visualizeData.Ottawa(
-#     DF.input = DF.ottawa
-#     );
-#
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-dashboard.files <- c(
-#   "dashboard-dummy-01",
-    "dashboard-dummy-02"
-    );
-
-for ( dashboard.file in dashboard.files ) {
-    rmarkdown::render(
-        input         = file.path(code.directory,paste0(dashboard.file,".Rmd")),
-        output_format = flexdashboard::flex_dashboard(),
-        output_file   = file.path(output.directory,paste0(dashboard.file,".html"))
-        );
-    }
-
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-vignette.files <- c(
-    "vignette-visualize-ottawa"
-    );
-
-for ( vignette.file in vignette.files ) {
-    rmarkdown::render(
-        input         = file.path(code.directory,paste0(vignette.file,".Rmd")),
-        output_format = "html_document",
-        output_file   = file.path(output.directory,paste0(vignette.file,".html"))
-        );
-    }
-
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 data.snapshot <- "2020-06-21.01";
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 DF.fatality.rates <- getData.wIFR(
     csv.wIFR.europe = file.path(data.directory,data.snapshot,"weighted-fatality-europe.csv"),
     csv.wIFR.canada = file.path(data.directory,data.snapshot,"weighted-fatality-canada.csv")
@@ -137,39 +96,16 @@ print( str(DF.serial.interval) );
 print( summary(DF.serial.interval) );
 print( sum(DF.serial.interval[,"fit"]) );
 
-list.raw.data <- getData.raw(
-    xlsx.ECDC                   = file.path(data.directory,data.snapshot,'raw-covid19-ECDC.xlsx'),
-    csv.JHU.cases               = file.path(data.directory,data.snapshot,'raw-covid19-JHU-cases.csv'),
-    csv.JHU.deaths              = file.path(data.directory,data.snapshot,'raw-covid19-JHU-deaths.csv'),
-    csv.GoCInfobase             = file.path(data.directory,data.snapshot,'raw-covid19-GoCInfobase.csv'),
-    xlsx.Ottawa.hospitalization = file.path(data.directory,data.snapshot,"Covid19_CODOttawaResidentHospitalAdmissionsByDay_EN.xlsx"),
-    xlsx.Ottawa.case.and.death  = file.path(data.directory,data.snapshot,"COVID-19_Ottawa_case_death_daily_count_data_EN.xlsx")
-    );
-print( names(list.raw.data) );
-
-list.patched.data <- patchData(
-    list.covid19.data = list.raw.data
-    );
-print( names(list.patched.data) );
-print( str(list.patched.data[['GoCInfobase']]) );
-
-DF.cross.check.JHU.GoCInfobase <- cross.check.JHU.GoCInfobase(
-    list.covid19.data = list.patched.data,
-    csv.output        = "diagnostics-compare-JHU-GoCInfobase-patched.csv"
-    );
-print(str(DF.cross.check.JHU.GoCInfobase));
-
-DF.covid19 <- getData.covid19(
-    retained.jurisdictions = jurisdictions,
-    list.covid19.data      = list.patched.data
-    );
-print( str(DF.covid19) );
-print( summary(DF.covid19) );
-
-print( unique( DF.covid19[,"jurisdiction"] ) );
-
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+data.snapshot <- "2020-11-04.01";
+
+DF.ottawa <- getData.Ottawa(
+    csv.input = file.path(data.directory,data.snapshot,"raw-covid19-Ottawa.csv")
+    );
+DF.ottawa.plot <- DF.ottawa;
+
 print( str(DF.ottawa) );
+print( summary(DF.ottawa) );
 
 colnames(DF.ottawa) <-gsub(
     x           = colnames(DF.ottawa),
@@ -195,15 +131,15 @@ DF.ontario[,'jurisdiction'] <- rep('ON',nrow(DF.ontario));
 DF.dummy <- rbind(DF.ottawa,DF.ontario);
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# results.wrapper.stan <- wrapper.stan(
-#     StanModel          = StanModel,
-#     FILE.stan.model    = FILE.stan.model,
-#     DF.covid19         = DF.dummy, # DF.ottawa, # DF.covid19,
-#     DF.fatality.rates  = DF.fatality.rates,
-#     DF.serial.interval = DF.serial.interval,
-#     forecast.window    = 14,
-#     DEBUG              = TRUE # FALSE
-#     );
+results.wrapper.stan <- wrapper.stan(
+    StanModel          = StanModel,
+    FILE.stan.model    = FILE.stan.model,
+    DF.covid19         = DF.dummy, # DF.ottawa, # DF.covid19,
+    DF.fatality.rates  = DF.fatality.rates,
+    DF.serial.interval = DF.serial.interval,
+    forecast.window    = 14,
+    DEBUG              = TRUE # FALSE
+    );
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 dashboard.files <- c(
