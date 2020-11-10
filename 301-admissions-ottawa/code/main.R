a5eaf295d3078a46d82572d2cf8dd7fe3fffb537
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-11-10 03:14:40 -0500

    updated: main.R

--- 301-admissions-ottawa/code/main.R
+++ 301-admissions-ottawa/code/main.R
@@ -138,7 +138,7 @@ results.wrapper.stan <- wrapper.stan(
     DF.fatality.rates  = DF.fatality.rates,
     DF.serial.interval = DF.serial.interval,
     forecast.window    = 14,
-    DEBUG              = TRUE # FALSE
+    DEBUG              = FALSE # TRUE
     );
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
